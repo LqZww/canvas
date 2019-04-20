c316477c9c20a3f11b6a1bb9eaaae6e99cf33c47
ZWW <951598077@qq.com>
2019-04-20 18:27:54 +0800

    更新

--- b.js
+++ b.js
@@ -15,7 +15,9 @@
 
 
 /*********控制宽高******/
-
+document.body.ontouchstart = function (eee) {
+    eee.preventDefault()
+}
 var yyy = document.getElementById('xxx');
 var context = yyy.getContext('2d');
 var lineWidth = 5
